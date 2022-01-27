4c74fe5677c3b19cacddabb36824aa04f915e114
Anusha Allaparthi <anusha.allaparthi@qentelli.com>
2022-01-27 12:14:33 +0530

    nodecrudapi

---
+++ server.js
@@ -0,0 +1,49 @@
+// Importing expressjs
+
+
+
+//express js for creating server and api to get,post,put and delete
+
+const express = require('express')
+
+//body Paersers for getting the data through the urls
+
+const bodyParser = require('body-parser')
+
+const {request} = require('express')
+
+//const app controls the entire app with express finctional constructor
+
+const app =express()
+
+//wee are saying express.js that to use body parser url encoded to be true
+
+app.use(bodyParser.urlencoded({extended : true}))
+
+//create route with creating the quote
+
+app.post('/quotes',(req,res) =>{
+
+    res.send(req.body)
+
+})
+
+
+
+app.get('/',(req,res)=>{
+
+    res.sendFile(__dirname + './index.html')
+
+})
+
+
+
+const PORT=3000
+
+
+
+app.listen(PORT,()=>{
+
+    console.log(`server running at port ${PORT}`)
+
+})
