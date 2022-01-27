b425159729aadc56c3ecdd2b754b8af30a9f2110
Anusha Allaparthi <anusha.allaparthi@qentelli.com>
2022-01-27 17:32:53 +0530

    Modified server.js file

--- server.js
+++ server.js
@@ -1,49 +1,49 @@
 // Importing expressjs
 
-
-
 //express js for creating server and api to get,post,put and delete
-
 const express = require('express')
 
-//body Paersers for getting the data through the urls
-
+//body Parser for getting the data through the urls
 const bodyParser = require('body-parser')
 
-const {request} = require('express')
+//Importing Mongo Client
+const MongoClient = require('mongodb').MongoClient
 
 //const app controls the entire app with express finctional constructor
-
+//server and api's
 const app =express()
 
 //wee are saying express.js that to use body parser url encoded to be true
-
 app.use(bodyParser.urlencoded({extended : true}))
 
-//create route with creating the quote
+//Db connection String
+const connectionString="mongodb+srv://Saianusha:Saianusha@cluster0.apekh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-app.post('/quotes',(req,res) =>{
+//connecting the db
+MongoClient.connect(connectionString,{useUnifiedTopology:true})
+    .then(client=>{
+         console.log('connected to database-server')
+         const db=client.db('star-wars-quotes')
+         const quotesCollection=db.collection('quotes')
 
-    res.send(req.body)
+         //create route with creating the quote
+                app.post('/quotes',(req,res) =>{
+                    quotesCollection.insertOne(req.body)
+                    .then(result=>{
+                        console.log (result)
+                    })
+                    .catch(error=>console.error(error))
+                    })
 
-})
-
+}).catch(console.error)  //promises
 
 
 app.get('/',(req,res)=>{
-
     res.sendFile(__dirname + './index.html')
-
 })
-
-
 
 const PORT=3000
 
-
-
 app.listen(PORT,()=>{
-
     console.log(`server running at port ${PORT}`)
-
 })
